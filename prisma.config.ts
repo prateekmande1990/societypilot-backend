3136942d2bc5e30d0d799adf5a9ebcb77a67d1da
Pratik Mande <prateekmande1990@gmail.com>
2026-05-11 13:46:04 +0530

    chore: initial project setup + prisma schema + basic structure
    - NestJS backend initialized
    - Prisma schema (Society, User, Flat)
    - Prisma Dev database configured
    - Basic app.module & main.ts setup

---
+++ prisma.config.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import { defineConfig, env } from "@prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+
+  // Optional: migration settings
+  migrations: {
+    path: "prisma/migrations",
+  },
+});
